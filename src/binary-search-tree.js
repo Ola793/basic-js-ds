728dac445264c87a5c8977213c77da6bc4e3ffb7
Olha Teplova <67291449+Ola793@users.noreply.github.com>
2023-04-15 14:14:58 +0200

    fix: binary-search-tree constructor

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,10 +1,5 @@
 const { Node } = require('../extensions/list-tree.js');
 
-/*
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
-
 class BinarySearchTree {
   constructor() {
     this.rootNumber = null;
